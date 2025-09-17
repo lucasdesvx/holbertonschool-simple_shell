9c56eb18b9995e60e19c9897f95d79867b972acb
Ryan364412 <bundhoor50@gmail.com>
2025-09-17 13:38:30 +0400

    m

---
+++ simpleshell/simple_shell.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+extern char **environ;
+int main(void)
+{
+    char *line = NULL;
+    size_t len = 0;
+    ssize_t nread;
+    pid_t pid;
+    int i;
+    while (1)
+    {
+        printf("shell is fun lol");
+        nread = getline(&line, &len, stdin);
+        if (nread == -1)
+        {
+            free(line);
+            printf("\n");
+            exit(0);
+        }
+        for (i = 0; line[i]; i++)
+        {
+            if (line[i] == '\n')
+            {
+                line[i] = '\0';
+                break;
+            }
+        }
+        pid = fork();
+        if (pid == 0)
+        {
+            char *argv[2];
+            argv[0] = line;
+            argv[1] = NULL;
+            if (execve(line,argv,environ) == -1)
+                perror("./shell");
+            exit(1);
+        }
+        else
+            wait(NULL);
+    }
+}
