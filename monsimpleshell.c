27a3112ba07bbce1e9ab49f45ddefd6ed8b46e1b
Your Name <you@example.com>
2026-01-11 17:50:58 +0000

    m

--- monsimpleshell.c
+++ monsimpleshell.c
@@ -5,6 +5,12 @@
 
 extern char **environ;
 
+/**
+ * main - Le simple shell
+ *
+ * Return: Always 0
+ */
+
 int main(void)
 {
 	char *ligne = NULL;
@@ -12,38 +18,37 @@ int main(void)
 	pid_t pid;
 	int status;
 	int i;
-	
+
 	while (1)
 	{
 		if (isatty(STDIN_FILENO))
 			write(1, "#cisfun$ ", 9);
-		
-		
+
 		if (getline(&ligne, &longueur, stdin) == -1)
 		{
 			write(1, "\n", 1);
 			free(ligne);
 			exit(0);
 		}
+
 		for (i = 0; ligne[i]; i++)
-			
 			if (ligne[i] == '\n')
 			{
 				ligne[i] = '\0';
 				break;
 			}
-		
+
 		if (ligne[0] == '\0')
 			continue;
-		
+
 		pid = fork();
 		if (pid == 0)
 		{
 			char *argv[2];
+
 			argv[0] = ligne;
 			argv[1] = NULL;
-			
-			
+
 			execve(ligne, argv, environ);
 			perror(ligne);
 			exit(1);
